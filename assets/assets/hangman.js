8eafaab34eccef2df8fbcffdd92ba9160b2d7d69
shazamba <Shaunshah24@gmail.com>
2018-01-03 23:55:42 -0500

    3rd=up

--- assets/assets/hangman.js
+++ assets/assets/hangman.js
@@ -1,49 +1,49 @@
 
-//array of buttons to show
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", 
-"I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
- "U", "V", "W", "X", "Y", "Z", "_"];
+// //array of buttons to show
+// var letters = ["A", "B", "C", "D", "E", "F", "G", "H", 
+// "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+//  "U", "V", "W", "X", "Y", "Z", "_"];
 
-//  var Stan = ["S", "T", "A", "N"]
+// //  var Stan = ["S", "T", "A", "N"]
 
-//  var Cartman = ["C","A","R","T","M","A","N"]
+// //  var Cartman = ["C","A","R","T","M","A","N"]
 
- var words = ['car', 'bus', 'boat', 'airplane'];
+//  var words = ['car', 'bus', 'boat', 'airplane'];
 
- var chosen = words[Math.floor(Math.random()* words.length)];
+//  var chosen = words[Math.floor(Math.random()* words.length)];
 
- start()
-console.log(chosen)
- function start(){
-    var usrGuess = '';
-    var i = 0;
-    var newarr = ''
-    var letters = [];
-     var dash =[];
-     for(var i = 0; i <chosen.length; i++){
-         dash.push('-')
-         letters.push(chosen[i])
-     }
-    document.getElementById('toGuess').innerHTML = dash;
-     console.log(dash)
-     console.log(letters)
+//  start()
+// console.log(chosen)
+//  function start(){
+//     var usrGuess = '';
+//     var i = 0;
+//     var newarr = ''
+//     var letters = [];
+//      var dash =[];
+//      for(var i = 0; i <chosen.length; i++){
+//          dash.push('-')
+//          letters.push(chosen[i])
+//      }
+//     document.getElementById('toGuess').innerHTML = dash;
+//      console.log(dash)
+//      console.log(letters)
 
-    document.onkeypress = function (e){
-        usrGuess = e.key;
-        i = letters.indexOf(usrGuess)
-    for(var y = 0; y < letters.length; y++){
-        //debugger;
-        if( letters[i] > -1 && letters[y] == letters[i]){
+//     document.onkeypress = function (e){
+//         usrGuess = e.key;
+//         i = letters.indexOf(usrGuess)
+//     for(var y = 0; y < letters.length; y++){
+//         //debugger;
+//         if( letters[i] > -1 && letters[y] == letters[i]){
             
-            newarr = dash.splice(i, 1, usrGuess)
-            document.getElementById('toGuess').innerHTML = newarr;
-        }
+//             newarr = dash.splice(i, 1, usrGuess)
+//             document.getElementById('toGuess').innerHTML = newarr;
+//         }
        
-    }
-        console.log(letters.indexOf(usrGuess));
-        console.log(e.key)
-    }
- }
+//     }
+//         console.log(letters.indexOf(usrGuess));
+//         console.log(e.key)
+//     }
+//  }
 
 
 
@@ -83,6 +83,94 @@ console.log(chosen)
 
 
 
+//document.getElementById('myBtn').addEventListener("click", function() {
+
+
+    start();
+    //});
+    
+    function start(){
+        var words = ['stan', 'kyle', 'cartman', 'randy'];
+        var info = pcChoice(); 
+        var score = 0;
+        var guess = 5;
+    
+        document.onkeypress = function (e){
+           compare(e.key);
+        }
+    
+        document.getElementById('score').innerHTML = score;
+        document.getElementById('guess').innerHTML = guess;
+    
+        function pcChoice() {
+            var chosen = words[Math.floor(Math.random()* words.length)];
+            var dash = '-';
+            var dashArr = [];
+            var letterArr = chosen.split('');
+            var result = [];
+    
+            for (var i=0; i < letterArr.length; i++){
+                dashArr.push(dash);
+            }
+    
+            toGuess.innerHTML = dashArr;
+            
+            result = [chosen, dashArr, letterArr];
+            
+            console.log(result);
+            console.log(info)
+            
+            return result;
+    
+    
+         }
+    
+        function compare(letter){
+            var dash = info[1];
+            var lettArr = info[2];
+            var found = [];
+    
+            for (var i = 0; i < lettArr.length; i++){
+                if(lettArr[i] == letter){
+                    found = lettArr[i];
+                    dash.splice(i, 1, found)
+                }
+            }
+    
+            var y = lettArr.indexOf(letter);
+            var z = dash.indexOf('-')
+            
+            
+            if ( y == -1){
+                guess-- 
+                document.getElementById('guess').innerHTML = guess;
+                
+            }else if(z == -1){
+                setTimeout(reset, 1000)
+                document.getElementById('toGuess').innerHTML = dash;
+                score++
+                document.getElementById('score').innerHTML = score;
+            }
+    
+            if( y == -1 && guess == 0) { 
+                guess = 5;
+                score = 0;
+                setTimeout(reset, 1000)
+                document.getElementById('score').innerHTML = score;
+                document.getElementById('toGuess').innerHTML = dash;
+                document.getElementById('guess').innerHTML = guess;
+    
+            }
+            toGuess.innerHTML = dash;
+           
+         }
+    
+         function reset(){
+            info = pcChoice()
+           
+    
+         }
+    }
 
 
 
